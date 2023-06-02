1ce60b5b43817f501b636954673d4aebb6082013
osama9ei <135328392+osama9ei@users.noreply.github.com>
2023-06-02 13:53:20 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -15,22 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['9647705475681'] //ur owner number
+global.ownername = "Boa hancock bot❤️" //ur owner name
+global.ytname = "YT: No" //ur yt chanel name
+global.socialm = "GitHub: OSAMA9EI" //ur github or insta name
+global.location = "Asia, Iraq, Salahalden" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "9647705475681" //ur number
+global.premium = ['9647705475681'] //ur premium number
+global.botname = '❤️BoaHancock Bot❤️' //ur bot name
+global.linkz = "https://youtube.com/" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://youtube.com/@enel.6136' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
+global.packname = "⍣بـوت بوا هانـكوك
+⍣" //ur sticker watermark packname
+global.author = "⍣8616228018846⍣" //ur sticker watermark author
 global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
 
 // Other
